9fb084986f5e61ab10e767c2fc77f0132b03c3ed
Ömer GÜZEL <58399384+omerrguzel@users.noreply.github.com>
2022-10-01 15:55:33 +0300

    Added Coroutine Experiment

--- app/src/main/java/com/oguzel/omer_guzel_week6_hw6/MainActivity.kt
+++ app/src/main/java/com/oguzel/omer_guzel_week6_hw6/MainActivity.kt
@@ -2,10 +2,33 @@ package com.oguzel.omer_guzel_week6_hw6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val counter : TextView = findViewById(R.id.textViewCounter)
+
+        while (true){
+            CoroutineScope(Dispatchers.IO).launch {
+                var i = 0
+//                while (true){
+                val answer = doNetworkCall()
+                withContext(Dispatchers.Main) {
+                    counter.text = i.toString()
+                    i++
+                    Log.v("PATIKA", answer)
+                }
+//                }
+            }
+        }
+    }
+    suspend fun doNetworkCall(): String {
+        delay(1000L)
+        return "Network Answer Called"
     }
 }
